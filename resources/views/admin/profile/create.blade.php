2f5c3485c00c44a12da8e0421a23bb84355746e0
daiki maruyama <maruden0103@gmail.com>
2021-09-03 07:02:40 +0000

    message

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -1,15 +1,12 @@
 <!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-      <title>Profile課題</title>
-    </head>
-    <body>
-        <div class="main.text">
-          <h1>Profile課題作成</h1>
+@extends('layouts.profile')
+@section('title', 'MY プロフィール')
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 mx-auto">
+                <h2>My プロフィール</h2>
+            </div>
         </div>
-    </body>
-</html>
+    </div>
+@endsection
