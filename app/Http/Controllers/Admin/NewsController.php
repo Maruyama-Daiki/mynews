3acbdc2db9fbe033f456409ffefb44d6c621ba38
daiki maruyama <maruden0103@gmail.com>
2021-09-09 15:07:37 +0000

    message

--- app/Http/Controllers/Admin/NewsController.php
+++ app/Http/Controllers/Admin/NewsController.php
@@ -5,17 +5,42 @@ namespace App\Http\Controllers\Admin;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
+use App\News;
+
 class newscontroller extends Controller
 {
-    //
      public function add()
   {
       return view('admin.news.create');
   }
   
-     public function create()
+    
+     public function create(Request $request)
   {
-      return view('admin.profile.create');
+      
+      $this->validate($request, News::$rules);
+​
+      $news = new News;
+      $form = $request->all();
+​
+      if (isset($form['image'])) {
+        $path = $request->file('image')->store('public/image');
+        $news->image_path = basename($path);
+      } else {
+          $news->image_path = null;
+      }
+​
+      
+      unset($form['_token']);
+    
+      unset($form['image']);
+​
+    
+      $news->fill($form);
+      $news->save();
+      
+      return redirect('admin/news/create');
   }
 }
+
 
