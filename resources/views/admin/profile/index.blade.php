bccc872364a7118c84aa9c5ae27e1892db47c490
daiki maruyama <maruden0103@gmail.com>
2021-09-18 13:48:39 +0000

    message

--- resources/views/admin/profile/index.blade.php
+++ resources/views/admin/profile/index.blade.php
@@ -43,8 +43,10 @@
                             @foreach($posts as $profile)
                                 <tr>
                                     <th>{{ $profile->id }}</th>
-                                    <td>{{ \Str::limit($profile->name, 100) }}</td>
-                                    <td>{{ \Str::limit($profile->gender, 100) }}</td>
+                                    <td>{{ \Str::limit($profile->name, 30) }}</td>
+                                    <td>{{ \Str::limit($profile->gender, 30) }}</td>
+                                    <td>{{ \Str::limit($profile->hobby, 200) }}</td>
+                                    <td>{{ \Str::limit($profile->introduction, 200) }}</td>
                                     <td>
                                         <div>
                                             <a href="{{ action('Admin\ProfileController@edit', ['id' => $profile->id]) }}">編集</a>
--- routes/web.php
+++ routes/web.php
@@ -30,6 +30,7 @@ Route::group(['prefix' => 'admin'], function() {
      Route::post('news/edit', 'Admin\NewsController@update')->middleware('auth'); // 追記
      Route::get('news/delete', 'Admin\NewsController@delete')->middleware('auth');
      Route::get('profile', 'Admin\ProfileController@index')->middleware('auth');
+     Route::get('profile/delete', 'Admin\ProfileController@delete')->middleware('auth');
     
 });
 
